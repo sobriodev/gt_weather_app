6de09295fe7294469fd9b73b2a70e71f86416c1f
Mateusz Kiełbasa <sobriodev@gmail.com>
2018-12-20 10:27:59 +0100

    Fixed negative temperature readout

--- parser.h
+++ parser.h
@@ -9,7 +9,7 @@ class Parser : public QObject
     Q_OBJECT
 
 public:
-    const QString SENSOR_DATA_FORMAT = "^\\[(T|H|P) [0-9]+\\.[0-9]+\\]$";
+    const QString SENSOR_DATA_FORMAT = "^\\[(T|H|P) -?[0-9]+\\.[0-9]+\\]$";
     explicit Parser(QObject *parent = nullptr);
 
     Q_INVOKABLE void parseData(QString data);
